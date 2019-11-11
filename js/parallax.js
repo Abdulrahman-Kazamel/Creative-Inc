c57886abc8e0f5b2c3042f04a40fb4a5766e7c81
Abdelrahman Kazamel <50080379+Kazamel@users.noreply.github.com>
2019-11-11 17:01:07 +0200

    Add files via upload

---
+++ js/parallax.js
@@ -0,0 +1,31 @@
+$(window).scroll(function() {
+  parallax();
+})
+
+function parallax() {
+
+  var wScroll = $(window).scrollTop()
+
+
+  $('.parallax--bg').css('background-position', 'center ' + (wScroll*0.75)+'px');
+
+
+  $('.parallax--box').css('top', -5 + (wScroll*.005)+'em')
+
+}
+
+// top button 
+var btn = $('#topBtn');
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function (e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, '300');
+});
